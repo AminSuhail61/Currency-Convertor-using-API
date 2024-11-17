299d18b91ac77633eea2c6ccafcfc5eb174ff02f
Mohd Amin Suhail <114796452+AminSuhail61@users.noreply.github.com>
2024-11-18 01:25:02 +0530

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const BASE_URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
-const BASE_URL ="https://latest.currency-api.pages.dev/v1/currencies/eur";
+const BASE_URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+// const BASE_URL ="https://latest.currency-api.pages.dev/v1/currencies/eur";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
